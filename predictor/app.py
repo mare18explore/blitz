857dd1bdea3af16d30fc88e906e5b5f75a8a0aac
mare18explore <abdimare914@gmail.com>
2026-07-24 01:54:09 -0600

    prep flask api for railway deployment

--- predictor/app.py
+++ predictor/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import pickle
+import pickle, os
 import numpy as np
 
 app = Flask(__name__)
@@ -67,4 +67,5 @@ def predict():
 })
 
 if __name__ == "__main__":
-  app.run(debug=True, port=5001)
+  port = int(os.environ.get("PORT", 5001))
+  app.run(debug=False, host="0.0.0.0", port=port)
